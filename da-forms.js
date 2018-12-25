92dc5f1ec02a2ac9336467168c8436dac3e83e90
Arthur Shlain <artur.slain@gmail.com>
2018-12-25 13:25:48 +0500

    Update da-forms.js

--- da-forms.js
+++ da-forms.js
@@ -7,26 +7,24 @@
     $body.submit('.da-form-ajax', function (event) {
 
         var _form = event.target;
-        var $form = $(event.target);
+        var form_id = $(_form).attr('id');
 
-        $('body').trigger('da-form-submit', $form);
-
-        if(!$form.hasClass('da-form')) return;
-        if(!$form.hasClass('da-form-ajax')) return;
+        if(!$(_form).hasClass('da-form')) return;
+        if(!$(_form).hasClass('da-form-ajax')) return;
 
         event.preventDefault();
 
         var formData = new FormData(_form);
-        var action = $form.attr('action');
-        var method = $form.attr('method');
+        var action = $(_form).attr('action');
+        var method = $(_form).attr('method');
 
         formData.append('json', '1');
 
-        $form.find('[type="submit"]').addClass('disabled progress-bar-striped progress-bar-animated');
-        $form.find('.da-form-response').html('');
-        $form.find('.da-form-group').removeClass(error_field_wrapper_class);
-        $form.find('.da-form-group [name]').removeClass(error_field_class);
-        $form.find('.invalid-feedback').remove();
+        $(_form).find('[type="submit"]').addClass('disabled progress-bar-striped progress-bar-animated');
+        $(_form).find('.da-form-response').html('');
+        $(_form).find('.da-form-group').removeClass(error_field_wrapper_class);
+        $(_form).find('.da-form-group [name]').removeClass(error_field_class);
+        $(_form).find('.invalid-feedback').remove();
 
         var xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
@@ -34,21 +32,20 @@
 
         xhr.onload = function (e) {
 
-            $form.find('[type="submit"]').removeClass('disabled progress-bar-striped progress-bar-animated');
+            $(_form).find('[type="submit"]').removeClass('disabled progress-bar-striped progress-bar-animated');
 
             var response = e.currentTarget.response;
 
-            var form_id = $(_form).attr('id');
-            var form_$parent = $(_form).parent();
-
             if(typeof response.form_html !== 'undefined'){
-                $form.replaceWith(response.form_html);
+                $(_form).replaceWith(response.form_html);
             }
 
-            $form = form_$parent.find('#' + form_id);
+            _form = '#' + form_id;
 
-            $('body').trigger('da-form-xhr-load', $form);
-
+            if(response.success){
+                $(_form).find('.hide-on-success').addClass('d-none');
+            }
+            
         };
         xhr.send(formData);
     });
